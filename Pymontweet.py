bf03b0b95da35fd5409b737371ede6fe3916d93e
Sulaksh M More <38561021+msulaksh@users.noreply.github.com>
2018-12-10 11:16:38 +0530

    Update Pymontweet.py

--- Pymontweet.py
+++ Pymontweet.py
@@ -14,34 +14,21 @@ ACCESS_TOKEN_SECRET = "TOKEN_SECRET"
  
  
 class StreamListener(tweepy.StreamListener):    
-    #This is a class provided by tweepy to access the Twitter Streaming API.
  
     def on_connect(self):
-        # Called initially to connect to the Streaming API
         print("You are now connected to the streaming API.")
     def on_error(self, status_code):
-        # On error - if an error occurs, display the error / status code
         print('An Error has occured: ' + repr(status_code))
         return False
     def on_data(self, data):
-        #This is the meat of the script...it connects to your mongoDB and stores the tweet
         try:
             client = MongoClient(MONGO_HOST)
-            
-            # Use twitterdb database. If it doesn't exist, it will be created.
             db = client.twitterdb
-    
-            # Decode the JSON from Twitter
             datajson = json.loads(data)
             
-            #grab the 'created_at' data from the Tweet to use for display
             created_at = datajson['created_at']
  
-            #print out a message to the screen that we have collected a tweet
             print("Tweet collected at " + str(created_at))
-            
-            #insert the data into the mongoDB into a collection called twitter_search
-            #if twitter_search doesn't exist, it will be created.
             db.twitter_search.insert(datajson)
         except Exception as e:
            print(e)
